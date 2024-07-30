468e888f6b9aaafd3cb0a8260cdf69da92276d2c
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2024-07-30 10:44:41 -0600

    ci(validator): :arrow_up: Implementacion de validador
    Se agregaron las librerias para validar campos class-validator y class-transformer
    
    package.json, package.lock.json, main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerInit } from './config/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
   swaggerInit(app);
+
   await app.listen(3000);
 }
 bootstrap();
