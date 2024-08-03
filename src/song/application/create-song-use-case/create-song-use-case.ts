81de3c6aa69e0dd97f68c83d5a4c70db576123a3
Isaac Castillo <isaac.castillo62055@uped.edu.sv>
2024-08-02 18:04:27 -0600

    feat(application): :white_check_mark: Implementacion de uso de envio de correo
    Implementando servicio de envio de email de ejemplo

--- src/song/application/create-song-use-case/create-song-use-case.ts
+++ src/song/application/create-song-use-case/create-song-use-case.ts
@@ -3,10 +3,14 @@ import { CreateSongDto } from './create-song.dto';
 import { SongRepository } from 'src/song/domain/repositories/song.repository';
 import { Injectable } from 'src/shared/dependencies/injectable';
 import { SongAlreadyExistException } from 'src/song/domain/exceptions/song-already.exist.exception';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class CreateSongUseCase {
-  constructor(private readonly songRepository: SongRepository) {}
+  constructor(
+    private readonly songRepository: SongRepository,
+    private readonly mailerService: MailerService,
+  ) {}
 
   async run(createSongDto: CreateSongDto): Promise<{ song: PrimitiveSong }> {
     const songExists = await this.songRepository.findByName(createSongDto.name);
@@ -16,6 +20,13 @@ export class CreateSongUseCase {
     }
     const song = Song.create(createSongDto);
     await this.songRepository.create(song);
+    await this.mailerService.sendMail({
+      to: 'test@gmail.com',
+      subject: 'Song created',
+      template: './welcomE',
+      context: song.toValue(),
+    });
+
     return { song: song.toValue() };
   }
 }
