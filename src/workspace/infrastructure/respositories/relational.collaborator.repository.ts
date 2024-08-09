ae894434831b2194f068478a6cee082a389819db
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2024-08-09 13:56:12 -0600

    refactor(collaborator): :recycle: Refactorizando consulta para obtener colaboradores
    Se refactorizo la consulta para aplicar una sola consulta profunda

--- src/workspace/infrastructure/respositories/relational.collaborator.repository.ts
+++ src/workspace/infrastructure/respositories/relational.collaborator.repository.ts
@@ -6,6 +6,7 @@ import RoleModel from '../models/role.model';
 import { UserMapper } from '../mappers/user.mapper';
 import { Injectable } from 'src/shared/dependencies/injectable';
 import { Op } from 'sequelize';
+import WorkspaceModel from '../models/workspace.model';
 
 @Injectable()
 export class RelationalCollaboratorRepository extends CollaboratorRepository {
@@ -21,22 +22,24 @@ export class RelationalCollaboratorRepository extends CollaboratorRepository {
 
     if (name) filter = { name: { [Op.iLike]: `%${name}%` } };
 
-    const workspaces = await this.collaboratorModel.findAll({
-      where: {
-        user_id: user,
-      },
-      attributes: ['workspace_id'],
-    });
-
     const usersCollaborators = await this.collaboratorModel.findAll({
-      where: {
-        workspace_id: workspaces.map((workspace) => workspace.workspace_id),
-        [Op.not]: {
-          user_id: user,
+      include: [
+        {
+          model: WorkspaceModel,
+          required: true,
+          include: [
+            {
+              model: this.collaboratorModel,
+              where: { user_id: user },
+            },
+          ],
         },
+        RoleModel,
+      ],
+      where: {
+        user_id: { [Op.ne]: user },
         ...filter,
       },
-      include: [RoleModel],
     });
 
     return usersCollaborators.map((collaborator) =>
