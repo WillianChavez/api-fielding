a8a1859995f7cb2804ccb5d6c3ce6b99963ba56c
willian <wachavez@innovacion.gob.sv>
2024-08-16 11:23:27 -0600

    refactor(dependencies): shared dependencies

--- src/shared/dependencies/index.ts
+++ src/shared/dependencies/index.ts
@@ -1,4 +1,2 @@
-import { Injectable } from './injectable';
-import { Transactional } from './transactional';
-
-export { Injectable, Transactional };
+export { Injectable } from './injectable';
+export { Transactional } from './transactional';
