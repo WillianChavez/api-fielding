5d59e77d372923d2b9a4d536cbb852312a667ec9
Francisco Santamaria <fbsantamaria@innovacion.gob.sv>
2024-08-19 14:37:43 -0600

    chore: add functionability

--- src/auth/domain/entities/user.entity.ts
+++ src/auth/domain/entities/user.entity.ts
@@ -2,53 +2,46 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export interface PrimitiveUser {
-    id: string;
-    name: string;
-    email: string;
-    createdAt?: Date;
-    deletedAt?: Date;
-    urlPhoto?: string;
+  id: string;
+  name: string;
+  email: string;
+  createdAt?: Date;
+  deletedAt?: Date;
+  urlPhoto?: string;
 }
 
-export interface UserAttributes {
-    id: string;
+export class User {
+  constructor(private attributes: PrimitiveUser & { password: string }) {}
+
+  static create(data: {
     name: string;
     email: string;
     password: string;
-    createdAt?: Date;
-    deletedAt?: Date | null;
-    urlPhoto?: string | null;
+    urlPhoto?: string;
+  }): User {
+    return new User({
+      id: uuidv4(),
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      createdAt: new Date(),
+      deletedAt: null,
+      urlPhoto: data.urlPhoto ?? null,
+    });
+  }
+
+//   getPassword(): string {
+//     return this.attributes.password;
+//   }
+
+  toValue(): PrimitiveUser {
+    return {
+      id: this.attributes.id,
+      name: this.attributes.name,
+      email: this.attributes.email,
+      createdAt: this.attributes.createdAt,
+      deletedAt: this.attributes.deletedAt,
+      urlPhoto: this.attributes.urlPhoto,
+    };
+  }
 }
-
-
-export class User {
-    constructor(private attributes: UserAttributes) {}
-
-    static create(data: { name: string; email: string; password: string; urlPhoto?: string }): User {
-        return new User({
-            id: uuidv4(),
-            name: data.name,
-            email: data.email,
-            password: data.password,
-            createdAt: new Date(),
-            deletedAt: null,
-            urlPhoto: data.urlPhoto ?? null
-        });
-    }
-
-    getPassword(): string {
-        return this.attributes.password;
-    }
-
-    toValue(): PrimitiveUser {
-        return {
-            id: this.attributes.id,
-            name: this.attributes.name,
-            email: this.attributes.email,
-            createdAt: this.attributes.createdAt,
-            deletedAt: this.attributes.deletedAt,
-            urlPhoto: this.attributes.urlPhoto
-        };
-    }
-}
-
