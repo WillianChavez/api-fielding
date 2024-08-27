048475e25b302d73297e83f259cad433e6718b77
Francisco Santamaria <fbsantamaria@innovacion.gob.sv>
2024-08-27 09:20:53 -0600

    chore: add decorator

--- src/shared/auth/decorators/get-user.decorator.ts
+++ src/shared/auth/decorators/get-user.decorator.ts
@@ -4,9 +4,11 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 
-export const GetUser = createParamDecorator((data, ctx: ExecutionContext) => {
-  const req = ctx.switchToHttp().getRequest();
-  const user = req.user;
-  if (!user) throw new InternalServerErrorException('User not found');
-  return user;
-});
+export const GetUser = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    const user = req.user;
+    if (!user) throw new InternalServerErrorException('User not found');
+    return !data ? user : user[data];
+  },
+);
